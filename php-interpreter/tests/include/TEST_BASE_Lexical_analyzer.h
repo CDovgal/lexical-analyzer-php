573b55dc520044a2b1df12895b1dd61dfc79043f
Kostiantyn.Dovgal <Kostiantyn.Dovgal@localhost>
2014-05-22 12:52:41 +0300

    refactor tests

--- php-interpreter/tests/include/TEST_BASE_Lexical_analyzer.h
+++ php-interpreter/tests/include/TEST_BASE_Lexical_analyzer.h
@@ -22,59 +22,65 @@ class PARAM_Lexical_analyzer
 {
 public:
   PARAM_Lexical_analyzer(const QString& i_filename, const TokensArray& i_tokens, bool i_file_existing = true);
-  
-  QString m_filename;
-  const TokensArray& m_tokens;
+  virtual ~PARAM_Lexical_analyzer() {}
+
+  virtual std::string toStdString() const;
+
   bool m_is_exist;
+  QString m_filename;
+  TokensArray m_tokens;
 };
 
-class TEST_ISource
+class TEST_ISourceCode
 {
 public:
+  virtual ~TEST_ISourceCode() {};
+
   virtual void Init() = 0;
 
-  virtual const PARAM_Lexical_analyzer* init_data() const = 0;
-  virtual const QString& source_code() const = 0;
+  virtual bool isFileExist() const = 0;
+  virtual QString filename() const = 0;
+  virtual const QString& sourceCode() const = 0;
+  virtual const TokensArray& expectedTokens() const = 0;
 };
 
-class TEST_Lexical_analyzer : public ::testing::TestWithParam< const PARAM_Lexical_analyzer* >, public TEST_ISource
+class TEST_Lexical_analyzer : public ::testing::TestWithParam< const PARAM_Lexical_analyzer* >, public TEST_ISourceCode
 {
 public:
   TEST_Lexical_analyzer();
-
-  virtual const PARAM_Lexical_analyzer* init_data() const;
-
-  QString filename() const;
-  bool is_exist() const;
-  const TokensArray& expectedTokens() const;
-
-  virtual const QString& source_code() const;
+  virtual ~TEST_Lexical_analyzer() {};
 
   virtual void Init();
+
+  virtual bool isFileExist() const;
+  virtual QString filename() const;
+  virtual const QString& sourceCode() const;
+  virtual const TokensArray& expectedTokens() const;
 
   virtual void SetUp();
   virtual void TearDown();
 
-  virtual std::string ToString() const;
+  virtual std::string toStdString() const;
 
 private:
   QString m_source_code;
 };
 
 #define DECLARE_TESTS(TESTNAME) \
-class TEST_AUTO_##TESTNAME : public TEST_Lexical_analyzer, public TEST_##TESTNAME{ \
+class AUTOTEST_##TESTNAME : public TEST_Lexical_analyzer, public TEST_##TESTNAME{ \
 public: \
-  TEST_AUTO_##TESTNAME() {} \
-  virtual ~TEST_AUTO_##TESTNAME() {} \
+  AUTOTEST_##TESTNAME() {} \
+  virtual ~AUTOTEST_##TESTNAME() {} \
   virtual void Init() { TEST_Lexical_analyzer::Init(); } \
-  virtual const PARAM_Lexical_analyzer* init_data() const { return TEST_Lexical_analyzer::init_data(); } \
-  virtual const QString& source_code() const { return TEST_Lexical_analyzer::source_code(); } \
+  virtual bool isFileExist() const{ return TEST_Lexical_analyzer::isFileExist(); }; \
+  virtual QString filename() const{ return TEST_Lexical_analyzer::filename(); }; \
+  virtual const QString& sourceCode() const{ return TEST_Lexical_analyzer::sourceCode(); }; \
+  virtual const TokensArray& expectedTokens() const{ return TEST_Lexical_analyzer::expectedTokens(); }; \
 };
 
 #define DECLARE_SPECIFIC_TEST(TESTNAME, TYPENAME) \
-  typedef TEST_AUTO_##TESTNAME TEST_AUTO_##TYPENAME##TESTNAME; \
-  TEST_P(TEST_AUTO_##TYPENAME##TESTNAME, ##TYPENAME) {\
-\
-  SCOPED_TRACE(ToString()); \
-  TEST_AUTO_##TESTNAME::Commence##TYPENAME(); \
+  typedef AUTOTEST_##TESTNAME AUTOTEST_##TYPENAME##TESTNAME; \
+  TEST_P(AUTOTEST_##TYPENAME##TESTNAME, ##TYPENAME) {\
+  SCOPED_TRACE(toStdString()); \
+  AUTOTEST_##TESTNAME::Commence##TYPENAME(); \
   }
--- php-interpreter/tests/src/TEST_Lexical_analyer.cpp
+++ php-interpreter/tests/src/TEST_Lexical_analyer.cpp
@@ -15,99 +15,53 @@
 
 #include "TEST_BASE_Lexical_analyzer.h"
 
-TEST(Lexical_analyzer, test_interface)
-{
-  QString source_path(".\\Data\\source.php");
-  QFile source_file(source_path);
-  if(!source_file.open(QIODevice::ReadOnly | QIODevice::Text))
-  {
-    ASSERT_TRUE(false) << "Source file " << source_path.toStdString() << " not found";
-  }
-
-  QString source_file_text("");
-  while (!source_file.atEnd())
-  {
-    source_file_text += source_file.readLine();
-  }
-
-  QVector<Token> expected_tokens;
-  expected_tokens.push_back(Token(E_TT_TAG,        "<?php",                2, 0));
-  expected_tokens.push_back(Token(E_TT_KEYWORD,    "if",                   3, 0));
-  expected_tokens.push_back(Token(E_TT_DELIMITER,  "(",                    3, 3));
-  expected_tokens.push_back(Token(E_TT_IDENTIFIER, "$a",                   3, 4));
-  expected_tokens.push_back(Token(E_TT_OPERATOR,   ">",                    3, 7));
-  expected_tokens.push_back(Token(E_TT_IDENTIFIER, "$b",                   3, 9));
-  expected_tokens.push_back(Token(E_TT_DELIMITER,  ")",                    3, 11));
-  expected_tokens.push_back(Token(E_TT_KEYWORD,    "echo",                 4, 2));
-  expected_tokens.push_back(Token(E_TT_CONSTEXPR,  "\"a bigger than b\"",  4, 7));
-  expected_tokens.push_back(Token(E_TT_DELIMITER,  ";",                    4, 24));
-  expected_tokens.push_back(Token(E_TT_TAG,        "?>",                   5, 0));
-
-  QVector<Token> actual_tokens;
-  LexicalAnalyzer analyzer(source_file_text);
-  for (Token temp; analyzer.nextToken(temp);)
-    actual_tokens.push_back(temp);
-
-  ASSERT_EQ(expected_tokens.size(), actual_tokens.size())
-    << "Mismatch of expected number of tokens and actual result";
-
-  std::equal(expected_tokens.cbegin(), expected_tokens.cend(), 
-               actual_tokens.cbegin(), [] (const Token& expected_token, const Token& actual_token) -> bool
-    {
-      EXPECT_EQ(expected_token, actual_token) << "Token mismatch\n";
-      return true;
-    }
-  );
-}
-
-
-class TEST_nextToken : public TEST_ISource
+class TEST_NextToken : public TEST_ISourceCode
 {
 public:
-  TEST_nextToken() {}
-  virtual ~TEST_nextToken() {}
+  TEST_NextToken() {}
+  virtual ~TEST_NextToken() {}
 
   virtual void CommencePositive()
   {
-    const auto& expected_tokens = init_data()->m_tokens;
+    EXPECT_EQ(1, 1);
 
-    //TokensArray actual_tokens;
-    //LexicalAnalyzer lex(source_code());
-    //for (Token token; lex.nextToken(token);)
-    //  actual_tokens.push_back(token);
+    TokensArray actual_tokens;
+    LexicalAnalyzer lex(sourceCode());
+    for (Token token; lex.nextToken(token);)
+      actual_tokens.push_back(token);
 
-    //ASSERT_EQ(expected_tokens.size(), actual_tokens.size())
-    //  << "Mismatch of expected number of tokens and actual result";
+    ASSERT_EQ(expectedTokens().size(), actual_tokens.size())
+      << "Mismatch of expected number of tokens and actual result";
 
-    //std::equal(expected_tokens.cbegin(), expected_tokens.cend(),
-    //  actual_tokens.cbegin(), [] (const Token& expected_token, const Token& actual_token) -> bool
-    //  {
-    //    EXPECT_EQ(expected_token, actual_token) << "Token mismatch\n";
-    //    return true;
-    //  }
-    //);
+    std::equal(expectedTokens().cbegin(), expectedTokens().cend(),
+      actual_tokens.cbegin(), [] (const Token& expected_token, const Token& actual_token) -> bool
+      {
+        EXPECT_EQ(expected_token, actual_token) << "Token mismatch\n";
+        return true;
+      }
+    );
   }
 };
 
-DECLARE_TESTS(nextToken)
+DECLARE_TESTS(NextToken)
 
-DECLARE_SPECIFIC_TEST(nextToken, Positive)
+DECLARE_SPECIFIC_TEST(NextToken, Positive)
 
+static const TokensArray tokensarray = {
+  Token(E_TT_TAG, "<?php", 2, 0)
+  , Token(E_TT_KEYWORD, "if", 3, 0)
+  , Token(E_TT_DELIMITER, "(", 3, 3)
+  , Token(E_TT_IDENTIFIER, "$a", 3, 4)
+  , Token(E_TT_OPERATOR, ">", 3, 7)
+  , Token(E_TT_IDENTIFIER, "$b", 3, 9)
+  , Token(E_TT_DELIMITER, ")", 3, 11)
+  , Token(E_TT_KEYWORD, "echo", 4, 2)
+  , Token(E_TT_CONSTEXPR, R"("a bigger than b")", 4, 7)
+  , Token(E_TT_DELIMITER, ";", 4, 24)
+  , Token(E_TT_TAG, "?>", 5, 0)
+};
 
-
-static const PARAM_Lexical_analyzer a(".\\Data\\source.php", {
-    Token(E_TT_TAG,           "<?php", 2, 0)
-  , Token(E_TT_KEYWORD,       "if", 3, 0)
-  , Token(E_TT_DELIMITER,     "(", 3, 3)
-  , Token(E_TT_IDENTIFIER,    "$a", 3, 4)
-  , Token(E_TT_OPERATOR,      ">", 3, 7)
-  , Token(E_TT_IDENTIFIER,    "$b", 3, 9)
-  , Token(E_TT_DELIMITER,     ")", 3, 11)
-  , Token(E_TT_KEYWORD,       "echo", 4, 2)
-  , Token(E_TT_CONSTEXPR,     "\"a bigger than b\"", 4, 7)
-  , Token(E_TT_DELIMITER,     ";", 4, 24)
-  , Token(E_TT_TAG,           "?>", 5, 0)
-}, true);
+static const PARAM_Lexical_analyzer a(".\\Data\\source.php", tokensarray, true);
 
 
 
@@ -115,6 +69,6 @@ static const PARAM_Lexical_analyzer* kFileParams_Positive[] = {
   &a
 };
 
-INSTANTIATE_TEST_CASE_P(nextToken,
-  TEST_AUTO_PositivenextToken,
+INSTANTIATE_TEST_CASE_P(NextToken,
+  AUTOTEST_PositiveNextToken,
   ::testing::ValuesIn(kFileParams_Positive));
