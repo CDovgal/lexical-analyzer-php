7fe050fe458a606ba48534a0dadf8c36308437ac
Kostiantyn.Dovgal <Kostiantyn.Dovgal@localhost>
2014-05-23 13:26:30 +0300

    refactor

--- php-interpreter/lib/src/LexicalAnalyzer.cpp
+++ php-interpreter/lib/src/LexicalAnalyzer.cpp
@@ -53,96 +53,50 @@ Token LexicalAnalyzer::next_token()
 {
   trim_spaces();
 
+  // check current state of analyzer
   if (m_state == E_STATE_NOT_INIT || m_state == E_STATE_FINISHED)
     return Token();
 
+  // save start position of next token
   m_token_pos = qMakePair(m_current_line, m_current_pos);
 
+  // we should work in scope of tag
   if (m_state == E_STATE_OUT_OF_TAG)
-  {
     if (shift_from_current(TAG_OPEN))
     {
       m_state = E_STATE_CODE;
       return Token(E_TT_TAG, TAG_OPEN, current_token_pos());
-    }
-    else
-    {
-      m_state = E_STATE_FINISHED;
-      return Token();
-    }
-  }
+    };
 
+  // extract string const expression
   if (current_symbol() == '"' || current_symbol() == '\'')
   {
     return extract_constexpr_str();
   }
 
-  QString str = m_source_lines[m_current_line].mid(m_current_pos, 2);
-  if (str == TAG_CLOSE)
-  {
-    increase_pos(2);
-    m_state = E_STATE_OUT_OF_TAG;
-    return Token(E_TT_TAG, TAG_CLOSE, current_token_pos());
-  }
+  // extract number const expression
+  if (current_symbol().isNumber())
+    return extract_constexpr_number();
 
-  if (str == COMMENT_OPEN)
-  {
-    increase_pos(2);
-    m_state = E_STATE_COMMENT;
-    return Token(E_TT_COMMENT, COMMENT_OPEN, current_token_pos());
-  }
-
-  if (str == COMMENT_LINE)
-  {
-    ++m_current_line;
-    m_current_pos = 0;
-    return Token(E_TT_COMMENT, COMMENT_LINE, current_token_pos());
-  }
-
-  if (m_state == E_STATE_COMMENT)
-  {
-    if (shift_from_current(COMMENT_END))
-    {
-      m_state = E_STATE_CODE;
-      return Token(E_TT_COMMENT, COMMENT_END, current_token_pos());
-    }
-    else
-    {
-      m_state = E_STATE_FINISHED;
-      return Token();
-    }
-  }
-
+  // extract variable identifier
   if (current_symbol() == '$')
   {
-    int temp_curr_pos = m_current_pos;
-    int temp_curr_line = m_current_line;
-
-    int var_start = increase_pos(1) - 1;
-
-    if ((current_symbol().isLetter() || current_symbol() == '_') && increase_pos(1))
+    QString var_str(current_symbol());
+    
+    // first symbol should be letter
+    if (increase_pos(1) && current_symbol().isLetter())
     {
-      for (; current_symbol().isLetterOrNumber() || current_symbol() == '_'; increase_pos(1));
-    }
-
-    if (m_current_line - temp_curr_line != 0)
-    {
-      temp_curr_pos = m_source_lines[temp_curr_line].length();
-    }
-    else
-    {
-      temp_curr_pos = m_current_pos;
-    }
-
-    QString var_str = m_source_lines[temp_curr_line].mid(var_start, temp_curr_pos - var_start);
-    if (var_str.length() > 1)
-    {
-      return Token(E_TT_IDENTIFIER, var_str, current_token_pos());
+      var_str.push_back(current_symbol());
+      // each next - letter, number or _
+      for (; increase_pos(1) && ( current_symbol().isLetterOrNumber() || current_symbol() == '_');)
+        var_str.push_back(current_symbol());
     }
     else
     {
       return Token(E_TT_ERROR, var_str, current_token_pos());
     }
+
+    return Token(E_TT_IDENTIFIER, var_str, current_token_pos());
   }
 
   if (current_symbol().isLetter())
@@ -156,6 +110,44 @@ Token LexicalAnalyzer::next_token()
           increase_pos(keyword.length());
           return Token(E_TT_KEYWORD, keyword, current_token_pos());
         }
+      }
+    }
+  }
+
+  {
+    QString str = m_source_lines[m_current_line].mid(m_current_pos, 2);
+    if (str == TAG_CLOSE)
+    {
+      increase_pos(2);
+      m_state = E_STATE_OUT_OF_TAG;
+      return Token(E_TT_TAG, TAG_CLOSE, current_token_pos());
+    }
+
+    if (str == COMMENT_OPEN)
+    {
+      increase_pos(2);
+      m_state = E_STATE_COMMENT;
+      return Token(E_TT_COMMENT, COMMENT_OPEN, current_token_pos());
+    }
+
+    if (str == COMMENT_LINE)
+    {
+      ++m_current_line;
+      m_current_pos = 0;
+      return Token(E_TT_COMMENT, COMMENT_LINE, current_token_pos());
+    }
+
+    if (m_state == E_STATE_COMMENT)
+    {
+      if (shift_from_current(COMMENT_END))
+      {
+        m_state = E_STATE_CODE;
+        return Token(E_TT_COMMENT, COMMENT_END, current_token_pos());
+      }
+      else
+      {
+        m_state = E_STATE_FINISHED;
+        return Token();
       }
     }
   }
@@ -186,16 +178,8 @@ Token LexicalAnalyzer::next_token()
         increase_pos(keyword.length());
         return Token(E_TT_OPERATOR, keyword, current_token_pos());
       }
-      else
-      {
-        Q_ASSERT(false);
-      }
     }
   }
-
-  if (current_symbol().isNumber())
-    return extract_constexpr_number();
-
 
   QString func_identifier;
   for (; !isEnd() && (current_symbol().isLetterOrNumber() || current_symbol() == '_' ); increase_pos(1))
@@ -258,40 +242,30 @@ int LexicalAnalyzer::increase_pos(int i_pos)
 {
   m_current_pos += i_pos;
 
-  //if (m_source_lines[m_current_line].length() < m_current_pos)
-  //{
-  //  Q_ASSERT(false);
-  //}
-
-  if (m_current_line < m_source_lines.length() && m_source_lines[m_current_line].length() <= m_current_pos)
+  if (m_source_lines[m_current_line].length() <= m_current_pos)
   {
     ++m_current_line;
     m_current_pos = 0;
   }
-
-  if (m_current_line == m_source_lines.length() /*|| 
-    ( (m_current_line == m_source_lines.length() - 1) && m_current_pos >= m_source_lines[m_current_line].length())*/
-    )
-    m_state = E_STATE_FINISHED;
 
   return m_current_pos;
 }
 
 Token LexicalAnalyzer::extract_constexpr_str()
 {
-  auto temp_pos = m_current_pos;
-  auto curr = current_symbol();
-  increase_pos(1);
-  m_current_pos = m_source_lines[m_current_line].indexOf(QString(curr), m_current_pos);
+  auto start = m_current_pos;
+  auto end = m_source_lines[m_current_line].indexOf(QString(current_symbol()), m_current_pos + 1);
 
-  if (m_current_pos != -1)
+  if (end != -1)
   {
-    increase_pos(1);
-    return Token(E_TT_CONSTEXPR, m_source_lines[m_current_line].mid(temp_pos, m_current_pos - temp_pos), m_current_line, temp_pos);
+    auto length = end - start + 1;
+    QString str = m_source_lines[m_current_line].mid(start, length);
+    increase_pos(length);
+    return Token(E_TT_CONSTEXPR, str, m_token_pos);
   }
   else
   {
-    return Token(E_TT_ERROR, "<error>", m_current_line, temp_pos);
+    return Token(E_TT_ERROR, "<error>", m_token_pos);
   }
 }
 
@@ -311,7 +285,7 @@ bool LexicalAnalyzer::isEnd() const
   if (m_current_line >= m_source_lines.length())
     return true;
 
-  if (m_current_line == m_source_lines.length() - 1 && m_current_pos == m_source_lines[m_current_line].length())
+  if (m_current_line == m_source_lines.length() - 1 && m_current_pos >= m_source_lines[m_current_line].length())
     return true;
 
   return false;
