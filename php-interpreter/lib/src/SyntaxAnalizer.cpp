61e6a895a2741c9341ab527b8adeee9201c44d49
Kostiantyn.Dovgal <Kostiantyn.Dovgal@localhost>
2014-05-27 22:10:15 +0300

    add cpp file for SyntaxAnalizer

---
+++ php-interpreter/lib/src/SyntaxAnalizer.cpp
@@ -0,0 +1,18 @@
+/**@file   SyntaxAnalyzer.cpp
+* @brief
+* @date    Date Created:: 27/05/14 Dovgal Kostiantyn
+*          $Date:: 27-05-2014 22:10#$
+* @version $Revision:: 1#$
+* @author  Copyright (c) 2014
+*/
+
+#include "SyntaxAnalizer.h"
+
+SyntaxAnalizer::SyntaxAnalizer()
+{}
+
+bool SyntaxAnalizer::readNextToken(const Token& i_token)
+{
+
+
+}
