89efdcffbc21adadae7f54804e5c64c9994d9de0
Kostiantyn.Dovgal_mac <Kostiantyn.Dovgal_mac@localhost>
2014-05-28 09:40:11 +0300

    c

--- php-interpreter/lib/include/SyntaxAnalizer.h
+++ php-interpreter/lib/include/SyntaxAnalizer.h
@@ -9,16 +9,71 @@
 #pragma once
 
 #include "Token.h"
+#include <QStack>
+#include <functional>
+#include <QStringList>
+#include "TokenSource.h"
+
+
+typedef QStringList ProductionResult;
+
+typedef QString Identifier;
+typedef QString Delimiter;
+typedef QString Variable;
+typedef QString Argument;
+
+typedef QStringList ArgumentList;
 
 enum SA_State : int;
 
-class SyntaxAnalizer
+class SyntaxAnalyzer
 {
 public:
-  SyntaxAnalizer();
+  SyntaxAnalyzer(const TokenSource& i_token_iter);
+  
+  ProductionResult readProduction();
 
-  bool readNextToken(const Token& i_token);
+  bool readNextToken(const Token& i_token, QString& o_info);
 
 private:
-  SA_State m_state;
+  
+  Token* next();
+  Token  token() const;
+  Token* prev();
+  
+  void readSubProduction(ProductionResult& io_production);
+  
+  bool readKeywordProduction(ProductionResult& io_production);
+  
+  bool readFunction(ProductionResult& io_production);
+  
+  bool readIdentifier(Identifier& io_identifier);
+  
+  bool readDelimiter(Delimiter& io_delimiter);
+  
+  bool readArgument(Argument& io_delimiter);
+  
+  bool readArgumentList(ArgumentList& io_delimiter);
+  
+  bool readVariable(Variable& io_variable);
+  
+  QString output(QString&& str);
+  QString output(const QString& str);
+  
+  TokenSource m_source;
+  
+  class ScopedDepthMeter
+  {
+  public:
+    ScopedDepthMeter(int& i_depth) : ref_depth(i_depth) {++ref_depth;}
+    ~ScopedDepthMeter() {--ref_depth;}
+    
+  private:
+    int& ref_depth;
+  };
+    
+  int m_depth;
 };
+
+#define SCOPED_DEPTH_METER \
+ScopedDepthMeter scope(m_depth);
--- php-interpreter/lib/src/SyntaxAnalizer.cpp
+++ php-interpreter/lib/src/SyntaxAnalizer.cpp
@@ -7,21 +7,161 @@
 */
 
 #include "SyntaxAnalizer.h"
+#include "LA_Aux.h"
 
-enum SA_State : int
+#define INCREMENT_LAYER inc_layer();
+#define DECREMENT_LAYER dec_layer();
+
+
+SyntaxAnalyzer::SyntaxAnalyzer(const TokenSource& i_token_source)
+  :  m_source(i_token_source)
+  ,  m_depth(0)
 {
-  SA_STATE_NOTHING = 0,
+}
 
-
-
-};
-
-SyntaxAnalizer::SyntaxAnalizer()
-  :  m_state(SA_STATE_NOTHING)
-{}
-
-bool SyntaxAnalizer::readNextToken(const Token& i_token)
+ProductionResult SyntaxAnalyzer::readProduction()
 {
+  ProductionResult result;
+  
+  readSubProduction(result);
+  
+  return result;
+}
 
+void SyntaxAnalyzer::readSubProduction(ProductionResult& io_production)
+{
+  if ( nullptr == next() )
+  {
+    return;
+  }
+  
+  switch (token().m_token_type)
+  {
+    case E_TT_TAG:
+      if (TAG_OPEN == token().m_lexem)
+      {
+        io_production.push_back(output(
+        "Program starts. Rules #100 applied. Changing to the rule 101."));
+      }
+      
+      if (TAG_CLOSE == token().m_lexem)
+      {
+        io_production.push_back(output(
+        "Program ends. Rules #100 applied."));
+      }
+    break;
+      
+    case E_TT_KEYWORD:
+      readKeywordProduction(io_production);
+    break;
+    
+      
+    default:
+    io_production.push_back(
+    "Something has gone wrong.");
+      break;
+  }
+  
+  return;
+}
+
+bool SyntaxAnalyzer::readKeywordProduction(ProductionResult& io_production)
+{
+  SCOPED_DEPTH_METER
+  
+  io_production.push_back(
+  "KEYWORDS");
+  
+  auto lexem = token().m_lexem;
+  
+  if (KEYWORD_FUNCTION == lexem)
+  {
+    return readFunction(io_production);
+  }
+
+  
+  
+
+  return false;
+}
+
+bool SyntaxAnalyzer::readFunction(ProductionResult& io_production)
+{
+  SCOPED_DEPTH_METER
+  
+  Identifier function_name;
+  if (!readIdentifier(function_name))
     return false;
+  
+  Delimiter open_round_bracket;
+  if (!readDelimiter(open_round_bracket) || BRACKET_ROUND_OPEN != open_round_bracket)
+    return false;
+  
+  
+  Delimiter close_round_bracket;
+  if (!readDelimiter(close_round_bracket) || BRACKET_ROUND_CLOSE != close_round_bracket)
+    return false;
+  
+  
+  return true;
+}
+
+bool SyntaxAnalyzer::readIdentifier(Identifier& io_identifier)
+{
+  SCOPED_DEPTH_METER
+  
+  if(!next())
+    return false;
+  
+  if (E_TT_IDENTIFIER == token().m_token_type)
+  {
+    io_identifier = token().m_lexem;
+    
+    return true;
+  }
+  
+  return false;
+}
+
+
+bool SyntaxAnalyzer::readDelimiter(Delimiter& io_delimiter)
+{
+  SCOPED_DEPTH_METER
+  
+  auto token = nextToken();
+  
+  if (E_TT_DELIMITER == token.m_token_type)
+  {
+    io_delimiter = token.m_lexem;
+    
+    return true;
+  }
+  
+  return false;
+}
+
+
+Token* SyntaxAnalyzer::next()
+{
+  return m_source.next();
+}
+      
+Token  SyntaxAnalyzer::token() const
+{
+  return m_source.token();
+}
+      
+Token* SyntaxAnalyzer::prev()
+{
+  return m_source.prev();
+}
+
+QString SyntaxAnalyzer::output(QString&& str)
+{
+  return QString(m_depth, '\t') + str;
+}
+
+QString SyntaxAnalyzer::output(const QString& str)
+{
+  return QString(m_depth, 't') + str;
 }
