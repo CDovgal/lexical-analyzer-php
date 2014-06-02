4621883b06e3d56d83d8974824e304fd698c20ab
Kostiantyn.Dovgal_mac <Kostiantyn.Dovgal_mac@localhost>
2014-06-03 01:33:42 +0300

    fix test
    file for Semantic interface

---
+++ php-interpreter/lib/include/SemanticAnalyzer.h
@@ -0,0 +1,17 @@
+//
+//  SemanticAnalyzer.h
+//  php-interpreter
+//
+//  Created by Dovgal Kostiantyn on 6/3/14.
+//
+//
+
+#pragma once
+
+#include "LexicalAnalyzer.h"
+
+class SemanticAnalysis
+{
+public:
+  SemanticAnalysis();
+};
