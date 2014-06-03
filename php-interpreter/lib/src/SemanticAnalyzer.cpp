e281bfae12edf20afb49dce2e2266cc088c11bf1
Kostiantyn.Dovgal <Kostiantyn.Dovgal@localhost>
2014-06-03 19:50:56 +0300

    add mock for semantic analyzer

---
+++ php-interpreter/lib/src/SemanticAnalyzer.cpp
@@ -0,0 +1,22 @@
+//
+//  SemanticAnalyzer.cpp
+//  php-interpreter
+//
+//  Created by Dovgal Kostiantyn on 6/3/14.
+//
+//
+
+#include "SemanticAnalyzer.h"
+
+SemanticAnalysis::SemanticAnalysis(const TokenSource& i_token_iter)
+  : m_source(i_token_iter)
+{}
+
+SemanticResult SemanticAnalysis::result()
+{
+  SemanticResult result;
+
+  // implementation
+
+  return result;
+}
