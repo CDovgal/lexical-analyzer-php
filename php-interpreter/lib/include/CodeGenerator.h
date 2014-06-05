8962ff2e5aabeee68794682ae6e860234753c7a0
Kostiantyn.Dovgal_win <Kostiantyn.Dovgal_win@localhost>
2014-06-05 05:53:47 +0300

    add counting function args improve init value checking semantic analyzer add file for code gen

---
+++ php-interpreter/lib/include/CodeGenerator.h
@@ -0,0 +1,10 @@
+/**@file   CodeGenerator.h
+* @brief
+* @date    Date Created:: 05/06/14 Dovgal Kostiantyn
+*          $Date:: 05-06-2014 05:52#$
+* @version $Revision:: 1#$
+* @author  Copyright (c) 2014
+*/
+
+#pragma once
+
